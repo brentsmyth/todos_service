d127bb86ce2ef715ec8a132834d3a6081e29a27c
Brent Smyth <brentsmyth8@gmail.com>
2023-04-28 23:14:29 -0400

    Refactoring auth with csrf protection, callback token as js var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def generate_csrf_token
+    session[:csrf_token] = SecureRandom.hex(32)
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_OAUTH_KEY'], ENV['GOOGLE_OAUTH_SECRET']
 end
 OmniAuth.config.allowed_request_methods = %i[get]
+OmniAuth.config.silence_get_warning = true
